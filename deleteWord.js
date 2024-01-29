1f0a5d3056bcb134f166c510b9c0e22a995aba75
maxfish711 <75544709+maxfish711@users.noreply.github.com>
2024-01-29 09:05:41 -0800

    adding lit charts button

--- deleteWord.js
+++ deleteWord.js
@@ -1,18 +1,27 @@
 async function detectButtonPress(){
-    var inputValue = document.getElementById("input1").value
+    inputValue = document.getElementById("input1").value
     var tabs = await chrome.tabs.query({
         active : true,
         currentWindow : true
     })
     var tabid = tabs[0].id
-    console.log(inputValue)
     chrome.scripting.executeScript({target:{tabId: tabid}, func : deletingKeyWord, args : [inputValue]})
+}
+
+async function litCharts(){
+    inputValue = "blur"
+    var tabs = await chrome.tabs.query({
+        active : true,
+        currentWindow : true
+    })
+    var tabid = tabs[0].id
+    chrome.scripting.executeScript({target:{tabId: tabid}, func : deletingKeyWord, args : [inputValue]})
+
 }
 
 function deletingKeyWord(a){
     var targetElement = document.getElementsByClassName(a)
     while(targetElement.length > 0){
-    console.log(targetElement)
     for (var i = 0; i < targetElement.length; i++){
         targetElement[i].classList.remove(a)
     }
@@ -22,4 +31,22 @@ function deletingKeyWord(a){
 }
 
 document.getElementById("button1").onclick = detectButtonPress
+document.getElementById("button2").onclick = litCharts
+
+
+currentTab = ""
+getCurrentTab()
+function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(queryOptions, ([tab]) => {
+      if (chrome.runtime.lastError)
+      console.error(chrome.runtime.lastError);
+      // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    //   callback(tab);
+        currentTab = tab
+    });
+  }
+  console.log(currentTab.url)
+
+
 
